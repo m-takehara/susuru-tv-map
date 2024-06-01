9f03a05d165c7b65b4d02fff173713e2c039384b
m-takehara <48828148+m-takehara@users.noreply.github.com>
2024-06-01 23:02:02 +0900

    Delete susuru-tv-importer/src/main/kotlin/me/takehara/gateway/InvalidPlaylistItemException.kt

--- susuru-tv-importer/src/main/kotlin/me/takehara/gateway/InvalidPlaylistItemException.kt
+++
@@ -1,8 +0,0 @@
-package me.takehara.gateway
-
-import com.google.api.services.youtube.model.PlaylistItem
-
-class InvalidPlaylistItemException(
-    override val message: String,
-    val playlistItem: PlaylistItem
-): Exception()
